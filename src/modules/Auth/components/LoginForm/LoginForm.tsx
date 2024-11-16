ea5ecfd169a7e3cf9b7e9e27b7a28241e7aacdf6
yura-tsihanovich <yurarehome@gmail.com>
2024-11-16 13:48:49 +0300

    add input text mode for password

--- src/modules/Auth/components/LoginForm/LoginForm.tsx
+++ src/modules/Auth/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,7 @@
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 // import styles from './ChangePasswordForm.module.scss';
-import commonStyles from '../../../../assets/css/commonStyles/CommonStyles.module.scss';
-import logo from '../../../../assets/images/icons/logo.svg';
+import commonStyles from "../../../../assets/css/commonStyles/CommonStyles.module.scss";
+import logo from "../../../../assets/images/icons/logo.svg";
 import Onboarding from "../../../../assets/images/Onboarding-img.png";
 import BackIcon from "../../../../assets/images/icons/goBackIcon.svg";
 import { Link, useHistory } from "react-router-dom";
@@ -25,80 +25,121 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div style={{ display: "flex", width: '100%', height: '100vh', position: 'relative' }}>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        position: "relative",
+      }}
+    >
       <div className={commonStyles.centeredWrapper}>
         <div className={commonStyles.navTop}>
-          <div onClick={() => history.goBack()} className={commonStyles.backBtnRelative}>
-            <img style={{marginRight: 9}} src={BackIcon} alt="Back arrow"/>
+          <div
+            onClick={() => history.goBack()}
+            className={commonStyles.backBtnRelative}
+          >
+            <img style={{ marginRight: 9 }} src={BackIcon} alt="Back arrow" />
             Back
           </div>
-          <div/>
+          <div />
         </div>
         <div className={commonStyles.centered}>
           <div className={commonStyles.login_logo}>
-            <img src={logo} alt="logo"/>
+            <img src={logo} alt="logo" />
           </div>
           <div className={commonStyles.logo_name}>Log In</div>
           <form onSubmit={handleSubmit(onSubmitForm)}>
             <div>
               <div className={commonStyles.inputWrapper}>
                 <input
-                    className={`${commonStyles.inputField} ${errors.email ? commonStyles.errorInput : ''}`}
-                    type="email"
-                    placeholder=""
-                    autoComplete="off"
-                    {...register("email", {
-                      required: "Please input your Email!",
-                      pattern: {
-                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                        message: "The input is not valid E-mail!"
-                      }
-                    })}
+                  className={`${commonStyles.inputField} ${
+                    errors.email ? commonStyles.errorInput : ""
+                  }`}
+                  type="email"
+                  placeholder=""
+                  autoComplete="off"
+                  {...register("email", {
+                    required: "Please input your Email!",
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "The input is not valid E-mail!",
+                    },
+                  })}
                 />
                 <label
-                    className={`${commonStyles.inputLabel} ${errors.email ? commonStyles.errorLabel : ""}`}>Email</label>
+                  className={`${commonStyles.inputLabel} ${
+                    errors.email ? commonStyles.errorLabel : ""
+                  }`}
+                >
+                  Email
+                </label>
               </div>
-              {errors.email && <p className={commonStyles.error}>{errors.email.message}</p>}
+              {errors.email && (
+                <p className={commonStyles.error}>{errors.email.message}</p>
+              )}
             </div>
             <div>
               <div className={commonStyles.inputWrapper}>
                 <input
-                    className={`${commonStyles.inputField} ${errors.email ? commonStyles.errorInput : ""}`}
-                    type="password"
-                    placeholder=""
-                    autoComplete="off"
-                    inputMode="numeric"
-                    {...register("password", {
-                      required: "Password must be at least 8 characters long!",
-                      minLength: {
-                        value: 8,
-                        message: "Password must be at least 8 characters long!"
-                      },
-                      pattern: {
-                        value: /^(?=.*[A-Z]).+$/,
-                        message: "Password must contain at least one uppercase letter!"
-                      }
-                    })}
+                  className={`${commonStyles.inputField} ${
+                    errors.email ? commonStyles.errorInput : ""
+                  }`}
+                  type="password"
+                  placeholder=""
+                  autoComplete="off"
+                  inputMode="text"
+                  {...register("password", {
+                    required: "Password must be at least 8 characters long!",
+                    minLength: {
+                      value: 8,
+                      message: "Password must be at least 8 characters long!",
+                    },
+                    pattern: {
+                      value: /^(?=.*[A-Z]).+$/,
+                      message:
+                        "Password must contain at least one uppercase letter!",
+                    },
+                  })}
                 />
                 <label
-                    className={`${commonStyles.inputLabel} ${errors.email ? commonStyles.errorLabel : ""}`}>Password</label>
+                  className={`${commonStyles.inputLabel} ${
+                    errors.email ? commonStyles.errorLabel : ""
+                  }`}
+                >
+                  Password
+                </label>
               </div>
-              {errors.password && <p className={commonStyles.error}>{errors.password.message}</p>}
+              {errors.password && (
+                <p className={commonStyles.error}>{errors.password.message}</p>
+              )}
             </div>
-            <Button variant="White" type="submit">Log In</Button>
+            <Button variant="White" type="submit">
+              Log In
+            </Button>
           </form>
         </div>
         <div className={commonStyles.footerCentered}>
           <div>
-            <div className={commonStyles.login_subtitle} style={{marginBottom: "15px"}}>
+            <div
+              className={commonStyles.login_subtitle}
+              style={{ marginBottom: "15px" }}
+            >
               Don’t have an account?
-              <Link style={{color: "#FFEA84", marginLeft: 8}} to="/auth/registration">
+              <Link
+                style={{ color: "#FFEA84", marginLeft: 8 }}
+                to="/auth/registration"
+              >
                 Sign Up
               </Link>
             </div>
             <div className={commonStyles.login_subtitle}>
               Forgot your password?
-              <Link style={{color: "#FFEA84", marginLeft: 8}} to="/auth/recover_password">
+              <Link
+                style={{ color: "#FFEA84", marginLeft: 8 }}
+                to="/auth/recover_password"
+              >
                 Recover
               </Link>
             </div>
@@ -106,7 +147,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
         </div>
       </div>
       <div className={commonStyles.sideOnboardingImg}>
-        <img style={{height: "100%"}} src={Onboarding} alt="Onboarding"/>
+        <img style={{ height: "100%" }} src={Onboarding} alt="Onboarding" />
       </div>
     </div>
   );
