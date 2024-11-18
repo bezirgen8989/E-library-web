5d0940e30d92439cbb79b712a39deb61014917ad
abia-win-ubuntu-wsl <yuri.tihanovich@brightorder.com>
2024-11-18 20:14:53 +0300

    add profile form

--- src/modules/UserManagement/components/ProfileUserComponent/ProfileUserComponent.tsx
+++ src/modules/UserManagement/components/ProfileUserComponent/ProfileUserComponent.tsx
@@ -3,6 +3,9 @@ import styles from "./ProfileUserComponent.module.scss";
 import { UserContext } from "../../../../core/contexts";
 import ProfileUserForm from "../ProfileUserForm";
 import { LanguageType } from "../ProfileUserForm/ProfileUserForm";
+import Edit from "../../../../assets/images/icons/editProfileIcon.svg";
+import Calendar from "../../../../assets/images/icons/calendarIcon.svg";
+import SmallBook from "../../../../assets/images/icons/smallBookIcon.svg";
 
 interface ProfileUserComponentProps {
   languages: LanguageType[];
@@ -21,6 +24,26 @@ const ProfileUserComponent: FC<ProfileUserComponentProps> = ({
           <img src={value?.photo?.link} alt="avatar" />
         </div>
         <div className={styles.profile_page_content}>
+          <div className={styles.userInfo}>
+            <div className={styles.userName}>
+              <span>{value?.userName}</span>
+              <div className={styles.editButton}>
+                <img src={Edit} alt="edit" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.userBirth}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img style={{ marginRight: 4 }} src={Calendar} alt="calendar" />
+              15.06.1998
+            </div>
+            <div
+              style={{ marginLeft: 14, display: "flex", alignItems: "center" }}
+            >
+              <img style={{ marginRight: 4 }} src={SmallBook} alt="book" />
+              74 completed books
+            </div>
+          </div>
           <ProfileUserForm languages={languages} onSubmit={onSubmit} />
         </div>
       </div>
--- src/modules/UserManagement/components/ProfileUserForm/ProfileUserForm.tsx
+++ src/modules/UserManagement/components/ProfileUserForm/ProfileUserForm.tsx
@@ -2,7 +2,9 @@ import styles from "./ProfileUserForm.module.scss";
 import React, { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import NoAvatar from "../../../../assets/images/icons/uploadBg.png";
+import tempAi from "../../../../assets/images/testAiImg.png";
 import LanguageModal from "../../../Auth/components/LanguageModal";
+import { Switch } from "antd";
 
 export type LanguageType = {
   id: number;
@@ -21,6 +23,7 @@ type FormValues = {
   userName: string;
   dateBirth: string;
   language: string;
+  bookLanguage: string;
   gender: string;
   photo: any;
 };
@@ -35,15 +38,21 @@ const ProfileUserForm: React.FC<RecoverProps> = ({
     flag: { link: NoAvatar },
   };
   const [selectedLanguage, setSelectedLanguage] = useState(defaultLanguage);
+  const [selectedBookLanguage, setSelectedBookLanguage] =
+    useState(defaultLanguage);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState<"language" | "bookLanguage">(
+    "language"
+  );
   const {
     control,
     handleSubmit,
     setValue,
-    // formState: {errors},
+    // formState: { errors },
   } = useForm<FormValues>({
     defaultValues: {
       language: defaultLanguage.name,
+      bookLanguage: defaultLanguage.name,
     },
   });
 
@@ -52,17 +61,25 @@ const ProfileUserForm: React.FC<RecoverProps> = ({
       const englishLanguage = languages.find((lang) => lang.name === "English");
       if (englishLanguage) {
         setSelectedLanguage(englishLanguage);
+        setSelectedBookLanguage(englishLanguage);
         setValue("language", englishLanguage.name);
+        setValue("bookLanguage", englishLanguage.name);
       }
     }
   }, [languages, setValue]);
 
   const onLanguageSelect = (language: LanguageType) => {
-    setSelectedLanguage(language);
-    setValue("language", language.name);
+    if (modalType === "language") {
+      setSelectedLanguage(language);
+      setValue("language", language.name);
+    } else if (modalType === "bookLanguage") {
+      setSelectedBookLanguage(language);
+      setValue("bookLanguage", language.name);
+    }
   };
 
-  const showModal = () => {
+  const showModal = (type: "language" | "bookLanguage") => {
+    setModalType(type);
     setIsModalOpen(true);
   };
 
@@ -74,10 +91,17 @@ const ProfileUserForm: React.FC<RecoverProps> = ({
       language: {
         id: selectedLanguage.id,
       },
+      bookLanguage: {
+        id: selectedBookLanguage.id,
+      },
       gender: data.gender,
     };
     console.log("profileFormattedData", formattedData);
     onSubmit(formattedData);
+  };
+
+  const onChange = (checked: boolean) => {
+    console.log(`switch to ${checked}`);
   };
 
   return (
@@ -93,7 +117,7 @@ const ProfileUserForm: React.FC<RecoverProps> = ({
                   <div
                     onMouseDown={(e) => {
                       e.preventDefault();
-                      showModal();
+                      showModal("language");
                     }}
                     className={styles.languageSelectWrapper}
                   >
@@ -107,17 +131,60 @@ const ProfileUserForm: React.FC<RecoverProps> = ({
                   </div>
                 )}
               />
-              <label className={styles.inputLabel}>Preferred Language</label>
+              <label className={styles.inputLabel}>App Language</label>
             </div>
           )}
         </div>
+        <div style={{ marginTop: 15 }}>
+          {languages.length > 0 && (
+            <div className={styles.inputWrapperLang}>
+              <Controller
+                name="bookLanguage"
+                control={control}
+                render={({ field }) => (
+                  <div
+                    onMouseDown={(e) => {
+                      e.preventDefault();
+                      showModal("bookLanguage");
+                    }}
+                    className={styles.languageSelectWrapper}
+                  >
+                    <div
+                      className={styles.languageSelect}
+                      style={{
+                        backgroundImage: `url(${selectedBookLanguage.flag.link})`,
+                      }}
+                    ></div>
+                    <span>{selectedBookLanguage.name}</span>
+                  </div>
+                )}
+              />
+              <label className={styles.inputLabel}>Book Language</label>
+            </div>
+          )}
+        </div>
+        <div className={styles.kidsSelectWrapper}>
+          <span>Kids Mode</span>
+          <Switch onChange={onChange} />
+        </div>
+        <div className={styles.aiWrapper}>
+          <div className={styles.aiAvatar}>
+            <img src={tempAi} alt="avatar" />
+          </div>
+          <span>AI Librarian</span>
+        </div>
+        {/*<Button variant="White" type="submit">*/}
+        {/*    Continue*/}
+        {/*</Button>*/}
       </form>
       <div style={{ height: "58px", width: "100%" }} />
       <LanguageModal
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
         languages={languages}
-        defaultLanguage={defaultLanguage}
+        defaultLanguage={
+          modalType === "language" ? selectedLanguage : selectedBookLanguage
+        }
         onLanguageSelect={onLanguageSelect}
       />
     </div>
