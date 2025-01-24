a8380cff5aac6d54ed10fbeafa1c7eb5f14f5a6a
abia-win-ubuntu-wsl <yuri.tihanovich@brightorder.com>
2025-01-24 13:18:26 +0300

    add new url

--- src/envConstants.ts
+++ src/envConstants.ts
@@ -1,7 +1,5 @@
 // export const urlSocket = "wss://av-stt-madlad-1811.plavno.app:14881";
-export const urlSocket = "wss://c16357690.plavno.app:50762";
-// export const urlSocket = "wss://c15602432.plavno.app:40102";
-//
+export const urlSocket = "wss://c16614136.plavno.app:21357";
 
 export const TERMS_URL = "https://hims.plavno.io/terms";
 export const PRIVACY_URL = "https://hims.plavno.io/privacy";
