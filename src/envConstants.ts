be0a014a7d8500a0e147ebf3824a7fa00cf94077
abia-win-ubuntu-wsl <yuri.tihanovich@brightorder.com>
2025-02-19 17:36:39 +0300

    change wss url

--- src/envConstants.ts
+++ src/envConstants.ts
@@ -1,5 +1,6 @@
 // export const urlSocket = "wss://av-stt-madlad-1811.plavno.app:14881";
-export const urlSocket = "wss://c16787597.plavno.app:54660";
+// export const urlSocket = "wss://c16787597.plavno.app:54660";
+export const urlSocket = "ws://185.150.27.254:30564";
 
 export const TERMS_URL = "https://hims.plavno.io/terms";
 export const PRIVACY_URL = "https://hims.plavno.io/privacy";
