a0dd8f7789207036dc4a684c72971d41c239915d
abia-win-ubuntu-wsl <yuri.tihanovich@brightorder.com>
2025-02-21 19:46:21 +0300

    add change env file add .gitignore file add necessary file names to .gitignore file

--- src/envConstants.ts
+++ src/envConstants.ts
@@ -1,6 +1,4 @@
-// export const urlSocket = "wss://av-stt-madlad-1811.plavno.app:14881";
-// export const urlSocket = "wss://c16787597.plavno.app:54660";
-export const urlSocket = "wss://185.150.27.254:30564";
+export const urlSocket = "wss://c18136649.plavno.app:55375";
 
 export const TERMS_URL = "https://hims.plavno.io/terms";
 export const PRIVACY_URL = "https://hims.plavno.io/privacy";
