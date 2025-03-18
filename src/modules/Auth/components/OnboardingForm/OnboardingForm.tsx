39af0af7d9de084a988a071e935124b979253481
Tatsiana Silchenko <tatiana.s@plavno.io>
2025-03-18 17:00:28 +0300

    remove social networks

--- src/modules/Auth/components/OnboardingForm/OnboardingForm.tsx
+++ src/modules/Auth/components/OnboardingForm/OnboardingForm.tsx
@@ -1,13 +1,13 @@
-import styles from "./OnboardingForm.module.scss";
+// import styles from "./OnboardingForm.module.scss";
 import commonStyles from "../../../../assets/css/commonStyles/CommonStyles.module.scss";
 import logo from "../../../../assets/images/icons/logo.svg";
 import Button from "../../../../components/common/Buttons/Button";
-import Social_1 from "../../../../assets/images/icons/facebook.svg";
-import Social_2 from "../../../../assets/images/icons/twitter.svg";
+// import Social_1 from "../../../../assets/images/icons/facebook.svg";
+// import Social_2 from "../../../../assets/images/icons/twitter.svg";
 import Onboarding from "../../../../assets/images/Onboarding-img.png";
-import GoogleIcon from "../../../../assets/images/icons/google.png";
-import { Link } from "react-router-dom";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+// import GoogleIcon from "../../../../assets/images/icons/google.png";
+// import { Link } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useLazySelector } from "../../../../hooks";
 
 type LoginFormProps = {
@@ -15,18 +15,18 @@ type LoginFormProps = {
 };
 
 const OnboardingForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
-  const handleGoogleSuccess = (credentialResponse: any) => {
-    console.log("Google User Token:", credentialResponse.credential);
-    onSubmit({ idToken: credentialResponse.credential });
-  };
+  // const handleGoogleSuccess = (credentialResponse: any) => {
+  //   console.log("Google User Token:", credentialResponse.credential);
+  //   onSubmit({ idToken: credentialResponse.credential });
+  // };
 
   const { result: localization } = useLazySelector(
     ({ auth }) => auth.appLocalization || {}
   );
 
-  const handleGoogleFailure = () => {
-    console.error("Google login failed");
-  };
+  // const handleGoogleFailure = () => {
+  //   console.error("Google login failed");
+  // };
 
   return (
     <GoogleOAuthProvider clientId="739797713062-bos5gumeckgjdsi5v3hhviiih5ttjrjn.apps.googleusercontent.com">
@@ -40,51 +40,51 @@ const OnboardingForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
             <Button variant="White" to="/auth/registration">
               {localization?.signUpWithEmail}
             </Button>
-            <div className={styles.googleButton}>
-              <div className={styles.googleButtonAbsolute}>
-                <img
-                  style={{ marginRight: "12px", marginTop: "-3px" }}
-                  src={GoogleIcon}
-                  alt="icon"
-                />
-                <div style={{ display: "inline-block", paddingTop: "3px" }}>
-                  Continue with Google
-                </div>
-              </div>
-              <GoogleLogin
-                onSuccess={handleGoogleSuccess}
-                onError={handleGoogleFailure}
-              />
-            </div>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                margin: "30px 0",
-              }}
-            >
-              <div className={styles.subtitle_line}></div>
-              <div className={commonStyles.login_subtitle}>
-                {localization?.orContinueWith}
-              </div>
-              <div className={styles.subtitle_line}></div>
-            </div>
+            {/*<div className={styles.googleButton}>*/}
+            {/*  <div className={styles.googleButtonAbsolute}>*/}
+            {/*    <img*/}
+            {/*      style={{ marginRight: "12px", marginTop: "-3px" }}*/}
+            {/*      src={GoogleIcon}*/}
+            {/*      alt="icon"*/}
+            {/*    />*/}
+            {/*    <div style={{ display: "inline-block", paddingTop: "3px" }}>*/}
+            {/*      Continue with Google*/}
+            {/*    </div>*/}
+            {/*  </div>*/}
+            {/*  <GoogleLogin*/}
+            {/*    onSuccess={handleGoogleSuccess}*/}
+            {/*    onError={handleGoogleFailure}*/}
+            {/*  />*/}
+            {/*</div>*/}
+            {/*<div*/}
+            {/*  style={{*/}
+            {/*    display: "flex",*/}
+            {/*    alignItems: "center",*/}
+            {/*    justifyContent: "center",*/}
+            {/*    margin: "30px 0",*/}
+            {/*  }}*/}
+            {/*>*/}
+            {/*  <div className={styles.subtitle_line}></div>*/}
+            {/*  <div className={commonStyles.login_subtitle}>*/}
+            {/*    {localization?.orContinueWith}*/}
+            {/*  </div>*/}
+            {/*  <div className={styles.subtitle_line}></div>*/}
+            {/*</div>*/}
 
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Link to="#" className={styles.soc_fb}>
-                <img src={Social_1} alt="Facebook logo" />
-              </Link>
-              <Link to="#" className={styles.soc_tw}>
-                <img src={Social_2} alt="Twitter logo" />
-              </Link>
-            </div>
+            {/*<div*/}
+            {/*  style={{*/}
+            {/*    display: "flex",*/}
+            {/*    alignItems: "center",*/}
+            {/*    justifyContent: "center",*/}
+            {/*  }}*/}
+            {/*>*/}
+            {/*  <Link to="#" className={styles.soc_fb}>*/}
+            {/*    <img src={Social_1} alt="Facebook logo" />*/}
+            {/*  </Link>*/}
+            {/*  <Link to="#" className={styles.soc_tw}>*/}
+            {/*    <img src={Social_2} alt="Twitter logo" />*/}
+            {/*  </Link>*/}
+            {/*</div>*/}
           </div>
           <div className={commonStyles.footerCentered}>
             <div className={commonStyles.login_subtitle}>
