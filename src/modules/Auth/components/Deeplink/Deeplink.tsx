3dd35c4e80042a0bdc53a6bfa706ab70c2d2a505
Tatsiana Silchenko <tatiana.s@plavno.io>
2025-03-19 16:49:52 +0300

    merge main

--- src/modules/Auth/components/Deeplink/Deeplink.tsx
+++ src/modules/Auth/components/Deeplink/Deeplink.tsx
@@ -3,6 +3,7 @@ import { FC, useEffect } from "react";
 import useAuthSocial from "hooks/useAuthSocial";
 
 const Deeplink: FC = () => {
+  console.log("Deeplink");
   const { finishSocialLogin } = useAuthSocial();
 
   useEffect(() => {
