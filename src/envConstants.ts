66db2e38fcb8cc5d22484db29a1815d2b1613940
Yura <yurarehome@gmail.com>
2025-03-20 23:19:52 -0700

    change url

--- src/envConstants.ts
+++ src/envConstants.ts
@@ -1,5 +1,5 @@
-// export const urlSocket = "wss://c18356234.plavno.app:40292";
-export const urlSocket = "wss://avatar18917229.plavno.app:12111";
+// export const urlSocket = "wss://avatar18917229.plavno.app:12111";
+export const urlSocket = "wss://avatar18949716.plavno.app:41686";
 
 export const TERMS_URL = "https://hims.plavno.io/terms";
 export const PRIVACY_URL = "https://hims.plavno.io/privacy";
