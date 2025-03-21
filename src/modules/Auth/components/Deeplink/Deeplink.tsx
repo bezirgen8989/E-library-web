c026030b0ac8f7ef02b781f814bd3860ffa6d8f7
Tatsiana Silchenko <tatiana.s@plavno.io>
2025-03-21 17:19:55 +0300

    fix socials redirects

--- src/modules/Auth/components/Deeplink/Deeplink.tsx
+++ src/modules/Auth/components/Deeplink/Deeplink.tsx
@@ -1,14 +1,8 @@
 import { FC, useEffect } from "react";
-
 import useAuthSocial from "hooks/useAuthSocial";
-import { useLazySelector } from "../../../../hooks";
 
 const Deeplink: FC = () => {
   const { finishSocialLogin } = useAuthSocial();
-
-  const userSate = useLazySelector(({ auth }) => auth);
-
-  console.log("userSate", userSate);
 
   useEffect(() => {
     finishSocialLogin();
